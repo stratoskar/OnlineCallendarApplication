03265ee25dc569d730551ceeba670c6b910f543e
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2022-06-07 18:53:45 +0300

    Add project files.

---
+++ CallendarApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CallendarApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
