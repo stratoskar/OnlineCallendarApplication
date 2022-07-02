28794cbbb912a52c1ba48f90fce47f9f99a25085
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2022-07-03 01:48:18 +0300

    Έφτιαξα καινούριο project
    έβαλα:
    -login και register σελίδες με κώδικες html,css,js
    -έφτιαξα την εφαρμογή να ξεκινάει από την σελίδα login
    -ενσωμάτωσα τον κώδικα του callendar που βρήκα στο github

---
+++ OnlineCallendarApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineCallendarApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCallendarApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // open Index page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // open Register page
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // open Login page
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
