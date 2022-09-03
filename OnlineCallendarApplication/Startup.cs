503d2906b85f646ade422a0b13317fda071bcecd
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2022-09-03 17:16:58 +0300

    allagoules

--- OnlineCallendarApplication/Startup.cs
+++ OnlineCallendarApplication/Startup.cs
@@ -21,7 +21,7 @@ namespace OnlineCallendarApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             // configure the DataBase postgreSQL
             services.AddDbContext<CallendarDataContext>(opt =>
