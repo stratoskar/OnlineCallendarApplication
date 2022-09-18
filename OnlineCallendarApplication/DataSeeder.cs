82651c856e4f91b7e5d7631d561a63267e2048c6
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2022-09-18 16:12:36 +0300

    clean unecessary code - part 2

--- OnlineCallendarApplication/DataSeeder.cs
+++ OnlineCallendarApplication/DataSeeder.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Callendar.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
